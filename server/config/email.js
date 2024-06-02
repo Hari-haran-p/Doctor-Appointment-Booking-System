a92965f4e4520d00d09611f133f90a96195e4b9c
PrashanthSai-K <srisathyasai24680@gmail.com>
2024-06-02 14:40:21 +0530

    email source updated

--- server/config/email.js
+++ server/config/email.js
@@ -1,12 +1,5 @@
-
 const nodemailer = require('nodemailer');
 require('dotenv').config();
-const {google} = require('googleapis');
-const OAuth2 = google.auth.OAuth2
-
-const OAuth2_client = new OAuth2(process.env.CLIENT_ID, process.env.CLIENT_SECRET)
-OAuth2_client.setCredentials({refresh_token:process.env.REFERESH_TOKEN})
-const access_token = OAuth2_client.getAccessToken();
 
 var transporter = nodemailer.createTransport({
     host: "sandbox.smtp.mailtrap.io",
