bd7806ebd22b3dff00885d06cac4c121f15f128b
PrashanthSai-K <saiprashanth.cs21@bitsathy.ac.in>
2024-06-03 15:31:22 +0530

    Doctor module minor updates

--- server/config/database.js
+++ server/config/database.js
@@ -1,6 +1,6 @@
 const {Sequelize } = require("sequelize");
 
-const sequelize  = new Sequelize("Doctor", "root", "passsword", {
+const sequelize  = new Sequelize("Doctor", "root", "password", {
     host: 'db',
     dialect:'mysql'
 });
